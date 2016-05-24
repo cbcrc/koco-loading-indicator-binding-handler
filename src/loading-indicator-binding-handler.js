a1a81d6eef5ee92171d85cc5b789ec4d22562d4e
Maxime Forget-Séguin <maxime.forget-seguin@radio-canada.ca>
2016-05-24 13:21:50 -0400

    update to koco2

--- src/loading-indicator-binding-handler.js
+++ src/loading-indicator-binding-handler.js
@@ -1,24 +1,24 @@
 // Copyright (c) CBC/Radio-Canada. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-define(['knockout', 'jquery'], function(ko, $) {
-    'use strict';
+import ko from 'knockout';
+import $ from 'jquery';
 
-    ko.bindingHandlers.loadingIndicator = {
-        update: function(element, valueAccessor /*, allBindings, viewModel, bindingContext */) {
-            var loading = ko.unwrap(valueAccessor());
-            var $element = $(element);
 
-            if (loading === true) {
-                $element.addClass('loading-indicator');
-            } else if (loading === false) {
+ko.bindingHandlers.loadingIndicator = {
+    update: function(element, valueAccessor /*, allBindings, viewModel, bindingContext */ ) {
+        var loading = ko.unwrap(valueAccessor());
+        var $element = $(element);
+
+        if (loading === true) {
+            $element.addClass('loading-indicator');
+        } else if (loading === false) {
+            $element.removeClass('loading-indicator');
+        } else if (loading.done) {
+            $element.addClass('loading-indicator');
+            loading.done(function() {
                 $element.removeClass('loading-indicator');
-            } else if (loading.done) {
-                $element.addClass('loading-indicator');
-                loading.done(function () {
-                    $element.removeClass('loading-indicator');
-                });
-            }
+            });
         }
-    };
-});
+    }
+};
